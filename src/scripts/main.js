e571679b275af03570956b6779dbb7cf31b564ca
Aleks <alex.andrutskii@gmail.com>
2019-08-09 20:07:31 +0300

    Changed a to button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-function toTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 document.addEventListener("DOMContentLoaded", function(event) {
+  function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
   document.getElementById("to-top").addEventListener("click", toTop);
 });
